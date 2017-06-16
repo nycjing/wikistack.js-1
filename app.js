1f20d159481c79f50b87dfc1e8663705d7e3f6d0
Lina Jones <lina.jones630@gmail.com>
2017-06-16 09:44:07 -0600

    added models/index

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+//Models are defined with sequelize.define('name', {attributes}, {options}).
 const express = require( 'express' );
+const sequelize = require('sequelize');
 const app = express();
 const router = express.Router();
 const morgan = require('morgan');
@@ -13,7 +15,6 @@ app.engine('html', nunjucks.render);
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
 app.use(bodyParser.json());
-
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
